7a71aa24a8f560afef57de18e06a97add68bea08
echuan <ezhy2008@gmail.com>
2022-08-13 18:57:08 +0800

    first commit sout

---
+++ src/main/java/cn/kgc/git/GitTest.java
@@ -0,0 +1,11 @@
+package cn.kgc.git;
+
+/**
+ * @author Echuan
+ * @create 2022-08-13-18:52
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("sout");
+    }
+}
