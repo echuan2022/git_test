0443b41b8320c662e043732adbce4006ef42f78a
echuan <ezhy2008@gmail.com>
2022-08-13 19:23:54 +0800

    branch1 commit2 changed

--- src/main/java/cn/kgc/git/GitTest.java
+++ src/main/java/cn/kgc/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git3");
         System.out.println("Hello Git normal merge");
+        System.out.println("Branch1 changed ");
     }
 }
