2c7350e3e33d5832be70a5045082d5eb9d5091f9
echuan <ezhy2008@gmail.com>
2022-08-13 19:02:59 +0800

    third commit hello git3

--- src/main/java/cn/kgc/git/GitTest.java
+++ src/main/java/cn/kgc/git/GitTest.java
@@ -6,6 +6,6 @@ package cn.kgc.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        System.out.println("Hello Git3");
     }
 }
